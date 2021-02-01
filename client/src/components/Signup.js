45a29fd104b994bb77afb5c486aebe168012f2b1
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-02-01 09:45:06 -0500

    sync push, modifatications in signup page

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -53,7 +53,7 @@ export default function Signup() {
               <div className="submit signup">
                 <button type="submit"><i className="fas fa-arrow-alt-circle-right"></i></button>
               </div>
-              <p className="message">Already a member? <a href="/user/signup">Login</a></p>
+              <p className="message">Already a member? <a href="/user/login">Login</a></p>
             </form>
 
             
