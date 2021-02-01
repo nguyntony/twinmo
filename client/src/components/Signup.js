fe53cde52b8dc9f47c642c2fa2f9bbef5a70f576
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-02-01 17:13:15 -0500

    added a icon to show whether the email and username is unique

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,5 +1,5 @@
 import signupImage from '../assets/signup.png'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import axios from 'axios';
 
 export default function Signup() {
@@ -13,22 +13,54 @@ export default function Signup() {
   const [email, setEmail] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [passwordCheck, setPasswordCheck] = useState('')
+  const [password2, setPassword2] = useState('')
   const [showMessage, setShowMessage] = useState(false) 
   const [alert, setAlert] = useState('default')
+  const [emailUnique, setEmailUnique] = useState(false)
+  const [usernameUnique, setUsernameUnique] = useState(false)
+  const [passwordCheck, setPasswordCheck] = useState(false)
 
-  const emailHandler = async (e) => {
-    console.log(email)
+  const emailCheckHandler = async (e) => {
+    // console.log(email)
     try {
-      const resp = await axios.post('/api/user/email-check', {
-        email
-      })
+      const resp = await axios.post('/api/user/email-check', {email})
       setShowMessage(true)
       setAlert('Email is already in use.')
     } catch (e) {
-      console.log(e)
+      if (email.includes('@')) {
+        setEmailUnique(true)
+      } else {
+        setEmailUnique(false)
+      }
     }
   }
+
+  const usernameCheckHandler = async (e) => {
+    try {
+      const resp = await axios.post('/api/user/username-check', {username})
+      setShowMessage(true)
+      setAlert('Username has already been taken.')
+    } catch (e) {
+      if (username.length !== 0) {
+        setUsernameUnique(true)
+      } else {
+        setUsernameUnique(false)
+      }
+    }
+  }
+
+  useEffect(()=> {
+    if (password === password2 && password2) {
+      setShowMessage(false)
+      setAlert('')
+      setPasswordCheck(true)
+    } else if (password !== password2) {
+      setShowMessage(true)
+      setAlert('Passwords do not match.')
+      setPasswordCheck(false)
+      
+    }
+  },[password2])
 
   return (
     <section className="contentContainer">
@@ -51,31 +83,39 @@ export default function Signup() {
 
               <div className="one-line">
                 <div className="field email">
-                  <input type="email" name="email" required autoComplete="off" placeholder=" " onChange={e => setEmail(e.target.value)} onBlur={emailHandler} onFocus={e => setShowMessage(false)}/>
+                  <input type="email" name="email" required autoComplete="off" placeholder=" " onChange={e => setEmail(e.target.value)} onBlur={emailCheckHandler} onFocus={e => setShowMessage(false)}/>
                   <label htmlFor="email" className="label">Email</label>
+                  
+                  {emailUnique && <p className="status"><i className="far fa-check-circle"></i></p>}
+                  
                 </div>
-                  {/* {!showMessage && <p>Email is already in use.</p>} */}
               </div>
 
               <div className="one-line">
                 <div className="field">
-                  <input type="text" name="username" required autoComplete="off" placeholder=" "/>
+                  <input type="text" name="username" required autoComplete="off" placeholder=" " onChange={e => setUsername(e.target.value)} onBlur={usernameCheckHandler} onFocus={e => setShowMessage(false)}/>
                   <label htmlFor="username" className="label">Username</label>
+
+                  {usernameUnique && <p className="status"><i className="far fa-check-circle"></i></p>}
                 </div>
               </div>
 
               <div className="one-line">
+              
                 <div className="field">
-                  <input type="password" name="password-checker" required autoComplete="off" placeholder=" "/>
-                  <label htmlFor="password-checker" className="label">Confirm Password</label>
-                </div>
-                <div className="field">
-                  <input type="password" name="password" required autoComplete="off" placeholder=" "/>
+                  <input type="password" name="password" required autoComplete="off" placeholder=" " onChange={e => setPassword(e.target.value)}/>
                   <label htmlFor="password" className="label">Password</label>
+                </div>
+
+                <div className="field">
+                  <input type="password" name="password-checker" required autoComplete="off" placeholder=" " onChange={e => setPassword2(e.target.value)}/>
+                  <label htmlFor="password-checker" className="label">Confirm Password</label>
+
+                  {passwordCheck && <p className="status"><i className="far fa-check-circle"></i></p>}
                 </div>
               </div>
               
-              {showMessage && <p className="alert">{alert}</p>}
+              {/* {showMessage && <p id="alert">{alert}</p>} */}
               
               <div className="submit signup">
                 <button type="submit"><i className="fas fa-arrow-alt-circle-right"></i></button>
