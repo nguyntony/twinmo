5f8593e16c018d10dbc7c3b9a8df443b39129959
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-02-08 15:00:16 -0500

    sync push

--- client/src/components/Member/FundsContext.js
+++ client/src/components/Member/FundsContext.js
@@ -4,9 +4,11 @@ export const FundsContext = createContext();
 
 export const FundsProvider = (props) => {
     const [updateFunds, setUpdateFunds] = useState(false)
+    const [toggle, setToggle] = useState(false)
+    const [test, setTest] = useState('default')
 
     return (
-        <FundsContext.Provider value={[updateFunds, setUpdateFunds]}>
+        <FundsContext.Provider value={{updateFundsContext: [updateFunds, setUpdateFunds], toggleContext: [toggle, setToggle], testContext: [test, setTest]}}>
             {props.children}
         </FundsContext.Provider>
     )
--- client/src/components/Member/Sidebar.js
+++ client/src/components/Member/Sidebar.js
@@ -8,8 +8,14 @@ export default function Sidebar() {
   const [successfulLogout, setSuccessfulLogout] = useState(false);
   const [userData, setUserData] = useState({})
   const [userFunds, setUserFunds] = useState('')
-  const [toggle, setToggle] = useState(true)
-  const [updateFunds, setUpdateFunds] = useContext(FundsContext)
+  // const [toggle, setToggle] = useContext(FundsContext)
+  // const [updateFunds, setUpdateFunds] = useContext(FundsContext)
+  // const [test, setTest] = useContext(FundsContext)
+
+  const {updateFundsContext, toggleContext, testContext} = useContext(FundsContext)
+  const [updateFunds, setUpdateFunds] = updateFundsContext
+  const [toggle, setToggle] = toggleContext
+  const [test, setTest] = testContext
 
   const location = useLocation()
   const currentPath = location.pathname
@@ -22,9 +28,14 @@ export default function Sidebar() {
     // color: '#ff99ab'
   }
 
+  const iconHighlight = {
+    color: '#ff778f'
+  }
+
   const dashboardHighlight = currentPath === '/member/home' ? {...highlight} : null
   const friendsHighlight = currentPath === '/member/friends' ? {...highlight} : null
   const payRequestHighlight = currentPath === '/member/pay-request' ? {...highlight} : null
+  const iconHome = currentPath === '/member/home' ? {...iconHighlight} : null
 
   const getUserData = async () => {
     const resp = await axios.get('/api/member/user-data');
@@ -51,19 +62,17 @@ export default function Sidebar() {
   }, [])
 
   useEffect(()=> {
-    console.log('Funds changed')
+    // console.log('Funds changed')
     getUserFunds()
+    console.log(`this is ${test}`)
   }, [updateFunds])
 
   return (
-    <section id="sidebar">
-
-      {/* <div className="collapsed-menu"></div> */}
-
-
+    <section className={toggle ? "sidebar open" : "sidebar"}>
 
         <div className="toggleIcon"><i className="fas fa-bars" onClick={toggleSidebar}></i></div>
-
+        
+        {toggle ? 
         <div className={toggle ? "sidebarContent active" : "sidebarContent"}>
           <div className="profilePicture">
             <img src={userData.profilePic} alt="profile pic"/>
@@ -86,10 +95,23 @@ export default function Sidebar() {
           <nav id="dashSubNav">
             <ul>
               <li><a href="#"><i className="fas fa-cog"></i></a></li>
-              <li><a onClick={processLogout}><i className="fas fa-sign-out-alt" onClick={processLogout}></i></a></li>
+              <li><a onClick={processLogout} className="logoutIcon"><i className="fas fa-sign-out-alt" onClick={processLogout}></i></a></li>
             </ul>
           </nav>
         </div>
+        :
+        
+          <nav id="quickLinks">
+            <ul>
+              <li><a style={iconHome} href="/member/home"><i className="fas fa-home"></i></a></li>
+              <li><a href="/member/friends"><i className="fas fa-user-friends"></i></a></li>
+              <li><a href="/member/pay-request"><i className="fas fa-hand-holding-usd"></i></a></li>
+              <li><a href="#"><i className="fas fa-money-check-alt"></i></a></li>
+              <li><a onClick={processLogout} className="logoutIcon quickLogout"><i className="fas fa-sign-out-alt" onClick={processLogout}></i></a></li>
+            </ul>
+          </nav>
+      
+        }
 
       <Switch>
         <Route path={currentPath} exact>
