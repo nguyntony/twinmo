0de141a913ca824cf610b01bd29c2b591d1ca6ac
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-02-09 17:14:52 -0500

    finished the history page

--- client/src/components/Member/Archive.js
+++ client/src/components/Member/Archive.js
@@ -3,14 +3,28 @@ import {useState, useEffect} from 'react'
 export default function Archive({img, date, name, description, amount, username, transactionDetail, archivedIcon, month, year}) {
 
   const [icon, setIcon] = useState('')
+  const [caption, setCaption] = useState('')
   const up = "fas fa-chevron-circle-up"
   const down = "fas fa-chevron-circle-down"
   const neutral = "fas fa-minus-circle"
+  const received = "received"
+  const sent = "sent"
+  const denied = "no-change"
+
 
   useEffect(()=> {
-    if (archivedIcon === "up") {setIcon(up)}
-    else if (archivedIcon === "down") {setIcon(down)}
-    else if (archivedIcon === "neutral") {setIcon(neutral)}
+    if (archivedIcon === "up") {
+      setIcon(up) 
+      setCaption(received)
+    }
+    else if (archivedIcon === "down") {
+      setIcon(down)
+      setCaption(sent)
+    }
+    else if (archivedIcon === "neutral") {
+      setIcon(neutral)
+      setCaption(denied)
+    }
   }, [])
 
   return (
@@ -19,9 +33,6 @@ export default function Archive({img, date, name, description, amount, username,
       <div className="archiveIcon">
         <div className="icon">
           <img src={img} alt="friend icon"/>
-        </div>
-        <div className="username">
-          {/* <p>@{username}</p> */}
         </div>
       </div>
 
@@ -33,11 +44,14 @@ export default function Archive({img, date, name, description, amount, username,
         <div className="description">
           <p><span className="date">{date}</span> {description}</p>
         </div>
+        <div className="type">
+          <p className="message">friend sent a request</p>
+        </div>
       </div>
 
       <div className="amount">
         <h4 className={archivedIcon}><i className={icon}></i>{amount}</h4>
-        <p>{transactionDetail}</p>
+        <p className={caption}>{transactionDetail}</p>
       </div>
     </div>
   )
